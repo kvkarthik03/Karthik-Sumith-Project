655905e9b7c70149a49b64b752173b2e7d14ea7e
kvkarthik03 <karthik.kv037@gmail.com>
2023-07-27 20:39:59 +0530

    Update Rooms, uncommit previous change
    Co-authored-by: Sumith T <Sumtan123@users.noreply.github.com>

--- hotel-booking/src/Rooms.jsx
+++ hotel-booking/src/Rooms.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useState } from 'react';
+//import {Modal, Button} from 'react-bootstrap'
 import {
   MDBCarousel,
   MDBCarouselItem,
@@ -7,10 +9,15 @@ import Button from 'react-bootstrap/esm/Button';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
-export default function Rooms() {
+function Rooms() {
+  const [showCarousel, setShowCarousel] = useState(false);
+
+  const handleImageClick = () => {
+    setShowCarousel(true);
+  };
   return (
-    <div>
-    <Navbar></Navbar>
+    // <div>
+    /* <Navbar></Navbar>
     <MDBCarousel showControls fade>
       <MDBCarouselItem
         className='w-100 d-block'
@@ -37,9 +44,58 @@ export default function Rooms() {
         alt='...'
       />
     </MDBCarousel>
-    <Button variant="primary">Book Now</Button>
-    
-    <Footer></Footer>
+    <Button variant="primary">Book Now</Button> */
+    <div>
+      <Navbar></Navbar>
+      <div className='container text-left'>
+        <div className='row text-left'>
+          <div className='col-md-4 text-left'>
+          <img
+              src='./room1.jpg'
+              className='smallimg'
+            />
+          </div>
+          <div className='col-md-7 text-left'>
+            <h1>Luxury Ambience Inn</h1>
+            <b>
+              <p>Rating: ⭐⭐⭐⭐</p>
+              <p>Room Type: Deluxe A/c</p>
+            </b>
+
+            <div style={{ float: 'right' }}>
+              <p>Price: Rs. 1000 per day</p>
+              <button className='btn btn-primary'>Book Now</button>
+            </div>
+          </div>
+
+
+        </div>
+
+      </div>
+
+      <Footer></Footer>
+
+      {/* <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal> */}
+
     </div>
   )
 }
+
+export default Rooms
